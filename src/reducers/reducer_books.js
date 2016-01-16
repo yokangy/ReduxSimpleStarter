cc5ddac85bb06498306a97d5a46a8b0481ff7972
Yondy Kang <yokangy@gmail.com>
2016-01-16 13:34:31 -0800

    Created a Redux reducer to return a list of books.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+  { title: 'The Fountainhead' },
+  { title: 'A Game of Thrones' },
+  { title: 'The Lean Startup' },
+  { title: 'Please Look After Mom' },
+  ]
+}
