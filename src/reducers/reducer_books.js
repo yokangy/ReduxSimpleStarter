894be4e9edac9d2338914be1b889c27a97190613
Yondy Kang <yokangy@gmail.com>
2016-01-17 19:17:42 -0800

    Created reducer for selected book state property.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-  { title: 'The Fountainhead' },
-  { title: 'A Game of Thrones' },
-  { title: 'The Lean Startup' },
-  { title: 'Please Look After Mom' },
+  { title: 'The Fountainhead', pages: 595 },
+  { title: 'A Game of Thrones', pages: 938 },
+  { title: 'The Lean Startup', pages: 281 },
+  { title: 'Please Look After Mom', pages: 303 },
   ]
 }
